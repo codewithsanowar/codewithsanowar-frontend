e20bfcc39cf79d5cd894ea44e88514efdc1c1f21
codewithsanowar <codewithsanowar@gmail.com>
2026-09-18 23:15:45 +0530

    Update AdminCourses.jsx

--- src/admin/AdminCourses.jsx
+++ src/admin/AdminCourses.jsx
@@ -48,7 +48,7 @@ const AdminCourses = ({ user }) => {
     myForm.append("price", price);
     myForm.append("createdBy", createdBy);
     myForm.append("duration", duration);
-    formData.append("image", imageFile);
+    myForm.append("image", file);
     myForm.append("language",language);
     myForm.append("oldprice",oldprice);
     myForm.append("lessons",lessons);
